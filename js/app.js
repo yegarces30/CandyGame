10976f704d05de2ea81bd732f76f34eb3a097120
Yeison Garcia Escobar <yegarces30@gmail.com>
2019-08-01 01:36:46 -0500

    Validación imagenes en línea

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ posFinal = null;
 continuaDraggable = 0;
 direccionAnterior = "";
 direccionActual = "";
+valorPuntuacion = 5;
+
+
 function modificarColorTitulo(){
   setTimeout(function(){
     $("#titulo").removeClass("main-titulo").addClass("main-titulo2");
@@ -20,6 +23,11 @@ function modificarColorTitulo(){
 }
 
 function llenarTablaInicial(){
+  $(".panel-tablero").html("");
+  minutos = 2;
+  segundos = 0;
+  $("#movimientos-text").text("0");
+  $("#score-text").text("0");
   var imagen = "";
   numeroCol = 0;
   id = "";
@@ -38,6 +46,7 @@ function llenarTablaInicial(){
 
       $(".panel-tablero").html($(".panel-tablero").html()+"<img id='"+id+"' class='imgCandy responsive-img' src='image/"+imagen+"' ></img>");
       $(".panel-tablero").css("height","100%");
+
     }
   }
    $( ".imgCandy" ).draggable(
@@ -53,8 +62,6 @@ function llenarTablaInicial(){
        drag: function() {
          if(continuaDraggable < 2){
            cambiarImagenesPosicion(this);
-         }else{
-           console.log("se metioooooooooooooo");
          }
        },
        stop: function() {
@@ -63,6 +70,30 @@ function llenarTablaInicial(){
         $( this ).draggable( "enable" );
         if(continuaDraggable == 1){
             volverPosicionOriginal(this,posImagenInicial.left,posImagenInicial.top);
+        }else if(continuaDraggable >=2){
+          noPermitirMovimiento(this);
+
+          nFila = parseInt(this.id[0]);
+          nCol = parseInt(this.id[2]);
+
+          validarEnLineaHorizontal(nFila);
+          if(nFila <7){
+            validarEnLineaHorizontal(nFila+1);
+          }
+          if(nFila > 0){
+            validarEnLineaHorizontal(nFila-1);
+          }
+
+          validarEnLineaVertical(nCol);
+          if(nFila <7){
+            validarEnLineaVertical(nCol+1);
+          }
+          if(nFila > 0){
+            validarEnLineaVertical(nCol-1);
+          }
+
+          animacionEliminar();
+
         }
 
         posImagenInicial = null;
@@ -72,7 +103,8 @@ function llenarTablaInicial(){
         continuaDraggable = 0;
         direccionAnterior = "";
         direccionActual = "";
-        $(this).draggable("option","axis","");
+        $(this).draggable("option","grid",[50,50]);
+
 
       }
 
@@ -84,9 +116,8 @@ function cambiarImagenesPosicion(imagen){
   dif = 0;
   if(posInicial.left != posFinal.left){
     direccionAnterior = direccionActual;
-    $(imagen).draggable("option","axis","x");
+    $(imagen).draggable("option","grid",[50,0]);
     if(direccionActual == direccionAnterior){
-      console.log("direcciones iguales");
       continuaDraggable++;
     }else{
       continuaDraggable--;
@@ -127,7 +158,7 @@ function cambiarImagenesPosicion(imagen){
 
         imagenActual = arregloPosiciones[x][y];
         arregloPosiciones[x][y] =  arregloPosiciones[x][y-1];
-        arregloPosiciones[x][y+1] = imagenActual;
+        arregloPosiciones[x][y-1] = imagenActual;
 
         pasarImagen(imagenOtra,posImagenInicial.left,2)
 
@@ -138,7 +169,7 @@ function cambiarImagenesPosicion(imagen){
     posInicial = posFinal;
   }else if(posInicial.top != posFinal.top){
     direccionAnterior = direccionActual;
-    $(imagen).draggable("option","axis","y");
+    $(imagen).draggable("option","grid",[0,50]);
     if(direccionActual == direccionAnterior){
       continuaDraggable++;
     }else{
@@ -190,7 +221,7 @@ function cambiarImagenesPosicion(imagen){
     }
     posInicial = posFinal;
   }
-  $( imagen ).draggable( "option", "grid" );
+  //$( imagen ).draggable( "option", "grid" );
 }
 
 function validarContinuacion(posX,posY){
@@ -213,6 +244,22 @@ function validarContinuacion(posX,posY){
   return false;
 }
 
+function noPermitirMovimiento(imagen){
+  switch (direccionActual) {
+    case "derecha":
+      var posLeft =  posImagenInicial.left
+      posLeft = posLeft + 100;
+      $(imagen).offset({left:posLeft});
+      break;
+    case "izquierda":
+      $(imagen).offset({left:posImagenInicial.left - 100});
+      break;
+    default:
+
+  }
+
+}
+
 function calcularTiempo(){
   var cadenaSegundos = "";
   if(segundos == 0){
@@ -225,6 +272,10 @@ function calcularTiempo(){
   if(minutos == 0 && segundos == 0){
     $( ".imgCandy" ).draggable( "destroy" );
     clearInterval(relog);
+    $("#titulo").text("Juego terminado");
+    $(".panel-tablero").animate({width: '0%'},1000);
+    $( ".panel-score" ).animate({width: '100%'}, 3000 );
+    $(".panel-tablero").css("border-width","0px");
   }
   cadenaSegundos = "0"+segundos;
   if(segundos>9){
@@ -264,14 +315,12 @@ function pasarImagen(imagen,posicion,opcion){
       setTimeout(function(){$(imagen).offset({left:(posicion)})},500);
       break;
     case 3:
-      console.log("imagen baja???");
       var topImg = imagen.offset().top;
       setTimeout(function(){$(imagen).offset({top:(topImg-30)})},100);
       setTimeout(function(){$(imagen).offset({top:(topImg-70)})},300);
       setTimeout(function(){$(imagen).offset({top:(posicion)})},500);
       break;
     case 4:
-      console.log("imagen sube???");
       var topImg = imagen.offset().top;
       setTimeout(function(){$(imagen).offset({top:(topImg+30)})},100);
       setTimeout(function(){$(imagen).offset({top:(topImg+70)})},300);
@@ -285,10 +334,91 @@ function volverPosicionOriginal(imagen,posicionX,posicionY){
   $(imagen).offset({left:posicionX,top:posicionY});
 }
 
+function validarEnLineaHorizontal(fila){
+  console.log("validando fila: "+fila);
+  contador = 0
+  id = ""
+  for (var i = 0; i < 8; i++) {
+    console.log("imagen:"+i);
+    for (var j = i; j < 8; j++) {
+      if(arregloPosiciones[fila][i] == arregloPosiciones[fila][j]){
+        contador++;
+        console.log("contando:"+contador);
+        id += "#"+fila+"-"+j+", ";
+      }else{
+        break;
+      }
+    }
+
+    if(contador >= 3){
+      $(id+"none").addClass("enLinea");
+      puntuacion = parseInt($("#score-text").text());
+      puntuacion += (valorPuntuacion * contador);
+      $("#score-text").text(puntuacion);
+    }
+    contador = 0
+    id = "";
+  }
+}
+
+function validarEnLineaVertical(columna){
+  console.log("validando columna: "+columna);
+  contador = 0
+  id = ""
+  for (var i = 0; i < 8; i++) {
+    for (var j = i; j < 8; j++) {
+      if(arregloPosiciones[i][columna] == arregloPosiciones[j][columna]){
+        contador++;
+        id += "#"+j+"-"+columna+", ";
+      }else{
+        break;
+      }
+    }
+
+    if(contador >= 3){
+      $(id+"none").addClass("enLinea");
+      puntuacion = parseInt($("#score-text").text());
+      puntuacion += (valorPuntuacion * contador);
+      $("#score-text").text(puntuacion);
+    }
+    contador = 0
+    id = "";
+  }
+}
+
+function animacionEliminar(){
+  tiempo = 0;
+  for (var i = 0; i < 5; i++) {
+    setTimeout(function(){
+      $(".enLinea").css("visibility","hidden");
+    },tiempo);
+    tiempo += 500;
+    setTimeout(function(){
+      $(".enLinea").css("visibility","visible");
+    },tiempo);
+    tiempo += 500;
+  }
+  setTimeout(function(){
+    $(".enLinea").css("visibility","hidden");
+  },tiempo);
+
+}
+
+
+function iniciar(){
+  clearInterval(relog);
+  $("#timer").html("02:00")
+  inicializarArregloPosiciones();
+  $(".btn-reinicio").text("Reiniciar");
+  $("#titulo").text("Match Game");
+  $(".panel-tablero").css("border-width","10px");
+  $(".panel-tablero").animate({width: '70%'},500);
+  $( ".panel-score" ).animate({width: '25%'}, 500 );
+  llenarTablaInicial();
+  relog = setInterval(calcularTiempo,1000);
+}
 
 $(document).ready(function(){
   setInterval(modificarColorTitulo, 1000);
-  inicializarArregloPosiciones();
-  llenarTablaInicial();
-  relog = setInterval(calcularTiempo,1000);
+  $(".btn-reinicio").click(iniciar);
 });
